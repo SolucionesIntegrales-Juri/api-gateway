572179d76af82e2fb455963baf55e583d78b4fc6
Alexander <u23241864@utp.edu.pe>
2025-12-02 08:06:59 -0500

    se migro a un repositorio independiente

---
+++ src/test/java/com/grupodos/alquilervehiculos/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupodos.alquilervehiculos.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
